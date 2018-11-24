3647958b1ede1d53b588e051da3af30d8c3d2930
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-11-25 00:22:35 +0800

    recursive function algorithm

--- recursive-vs-non-recursive/main.cpp
+++ recursive-vs-non-recursive/main.cpp
@@ -6,6 +6,41 @@
 using namespace std::chrono_literals;
 using int_array = std::vector<int>;
 
+void quick_sort(const int_array::iterator begin, const int_array::iterator end)
+{
+	if(end - begin > 1)
+	{
+		const auto median = *begin;
+
+		const auto lower_bound_begin = begin;
+		const auto upper_bound_end = end;
+
+		auto lower_bound_end = lower_bound_begin + 1;
+
+		for(auto it = lower_bound_end; it != end; ++it)
+		{
+			const auto value = *it;
+			if(value <= median)
+			{
+				*it = *lower_bound_end;
+				*lower_bound_end = value;
+				++lower_bound_end;
+			}
+		}
+		const auto lower_bound_last = lower_bound_end - 1;
+		*begin = *lower_bound_last;
+		*lower_bound_last = median;
+
+		quick_sort(lower_bound_begin, lower_bound_last);
+		quick_sort(lower_bound_end, upper_bound_end);
+	}
+}
+
+void quick_sort(int_array& array)
+{
+	quick_sort(array.begin(), array.end());
+}
+
 int main()
 {
 	constexpr auto num_elements = 8'000'000;
@@ -23,6 +58,8 @@ int main()
 	}
 
 	const auto start_point = std::chrono::steady_clock::now();
+
+	quick_sort(array);
 
 	const auto end_point = std::chrono::steady_clock::now();
 
